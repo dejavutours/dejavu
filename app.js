5098520c10b475d81ce3f62b4fa2068233f5ab1b
dejavutours <panchalchintanb@gmail.com>
2021-10-02 00:06:17 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,156 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
+const multer = require("multer");
+const helmet = require("helmet");
+const compression = require("compression");
+const { uploadFile } = require("./s3");
+const { uploadFileProof } = require("./s3");
+const PORT = process.env.PORT || 5000;
+
+const Tours = require("./models/tours");
+const User = require('./models/user');
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.ogxnm.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+const app = express(); 
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
+
+  const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      if(req.url == "/booktrip"){
+        cb(null, "images/proofs");
+      }else if (req.url == "/admin/addblog") {
+        cb(null, "images/blog");
+      }
+      else if (req.url == "/admin/addEditedblog") {
+        cb(null, "images/blog");
+      }
+      else{
+        cb(null, "images");
+      }  
+    },
+    filename: (req, file, cb) => {
+      if (req.url == "/booktrip"){
+        cb(
+          null,
+          new Date().toISOString().replace(/:/g, "-") + '-' + file.originalname
+        );
+      }else if (req.url == "/admin/addblog") {
+        cb(
+          null,
+          new Date().toISOString().replace(/:/g, "-") + '-' + file.originalname
+        );
+      }
+      else if (req.url == "/admin/addEditedblog") {
+        cb(
+          null,
+          new Date().toISOString().replace(/:/g, "-") + '-' + file.originalname
+        );
+      }  
+      else{
+        cb(
+          null,
+          file.originalname
+        );
+      }
+    },
+  });
+
+  const fileFilter = (req, file, cb) => {
+    if (
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpg" ||
+      file.mimetype === "image/jpeg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  };
+
+  app.set("view engine", "ejs");
+  app.set("views", "views");
+
+  const tourRoutes = require("./routes/tours");
+
+  //app.use(helmet());
+  app.use(compression());
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single("image"));
+
+  // app.use(async(req, res, next) => {
+  //   if(!req.file){
+  //     return next();
+  //   }else{
+  //     if(req.url != '/booktrip') {
+  //       const result = await uploadFile(req.file);
+  //       return next();
+  //      }else{
+  //       const result = await uploadFileProof(req.file);
+  //       return next();
+  //      }
+  //   }
+  
+  // });
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection);
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  //console.log(res.locals.csrfToken);
+  next();
+});
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.use(tourRoutes);
+
+
+mongoose
+  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false })
+  .then((result) => {
+    console.log("connected to DB at port 5000");
+    app.listen(PORT);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ routes/tours.js
@@ -0,0 +1,121 @@
+const express = require("express");
+
+const Tours = require("../models/tours");
+
+const toursController = require("../controllers/tours");
+
+const isAuth = require('../middleware/is-auth');
+
+const router = express.Router();
+
+router.get("/" , toursController.getIndexPage);
+
+router.get("/admin/addtours" , isAuth, toursController.getAddTours);
+
+router.post("/admin/addtours", isAuth,  toursController.postAddTours);
+
+router.get("/tripdetails/:token" , toursController.getTourDetails);
+
+router.post("/newsletter" , toursController.postNewsletter);
+
+router.get("/contact", toursController.getContact);
+
+router.post("/contact", toursController.postContact);
+
+router.get("/about", toursController.getAbout);
+
+router.post("/getbooktrip", toursController.getBookTrip);
+
+router.post("/booktrip", toursController.postBookTrip);
+
+router.post("/admin/delete", isAuth, toursController.postDelete);
+
+router.post("/admin/edit", isAuth, toursController.postEdit);
+
+router.post("/admin/addeditedtours", isAuth, toursController.postAddeditedtours);
+
+router.post("/admin/addupcoming", isAuth, toursController.postAddUpcoming);
+
+router.get("/upcomingtrips", toursController.getUpcomingTrips);
+
+//router.post("/admin/deleteupcoming", isAuth, toursController.postDeleteUpcomingtrips);
+
+router.get("/accomodation", toursController.getAccomodation);
+
+router.get("/admin/addaccomodation", isAuth, toursController.getAddAccomodation);
+
+router.post("/admin/addaccomodation", isAuth, toursController.postAddAccomodation);
+
+router.post("/admin/accod/delete", isAuth, toursController.postDeleteAccomodations);
+
+router.post("/admin/accod/edit", isAuth, toursController.postEditAccomodations);
+
+router.post("/admin/accod/addededit", isAuth, toursController.postAddEditAccomodations);
+
+router.get("/accomodationdetails/:token", toursController.getAccomodationsDetails);
+
+router.get("/filters/:token", toursController.getStateFilters);
+
+router.post("/admin/AddImage", isAuth, toursController.getToursAddimage);
+
+router.post("/admin/ImageAdded", isAuth, toursController.postImageAdded);
+
+router.post("/admin/BannerImageAdded", isAuth, toursController.postBannerImageAdded);
+
+router.post("/admin/deleteimage", isAuth, toursController.postDeleteimage);
+
+router.get("/admin/login", toursController.getAdminLogin);
+
+router.post("/admin/login", toursController.postAdminLogin);
+
+router.post("/admin/logout", isAuth, toursController.postLogout);
+
+router.post("/accod/queryform", toursController.postAccodQuery);
+
+router.get("/admin/accoddetails", isAuth, toursController.getAccodDetails);
+
+router.get("/admin/viewregistration", isAuth, toursController.getViewRegistration);
+
+router.get("/admin/viewmails" , isAuth, toursController.getViewEmails);
+
+router.post("/admin/delaccoddetails", isAuth, toursController.postDeleteAccod);
+
+router.get("/admin/downloadpdf/:regid", isAuth, toursController.getDownloadPDF);
+
+router.get("/admin/code", isAuth, toursController.getAdminCode);
+
+router.post("/admin/accod/AddImage", isAuth, toursController.getAccodAddImage);
+
+router.post("/admin/accod/ImageAdded", isAuth, toursController.postAccodImageAdded);
+
+router.post("/admin/accod/deleteimage", isAuth, toursController.postDeleteAccodImage);
+
+router.post("/admin/accod/BannerImageAdded", isAuth, toursController.postAccodBannerImageAdded);
+
+router.post("/admin/deleteupcomingdate", isAuth, toursController.postDeleteUpcomingDate);
+
+router.post("/bookdate", toursController.postBookDate);
+
+router.post("/admin/deleteregistration", toursController.postDeleteRegistration);
+
+router.get("/blog", toursController.getBlog);
+
+router.get("/blogDetails/:id/:title", toursController.getSingleBlog);
+
+router.get("/admin/addblog", isAuth, toursController.getAddBlog);
+
+router.post("/admin/addblog", isAuth, toursController.postAddBlog);
+
+router.get("/tags/:id", toursController.getTagFilter);
+
+router.post("/search/trip", toursController.getSearchTrip);
+
+router.post("/search/blog", toursController.getSearchBlog);
+
+router.post("/admin/deleteblog", isAuth, toursController.postDeleteBlog);
+
+router.post("/admin/editblog", isAuth, toursController.getEditBlog);
+
+router.post("/admin/addEditedblog", isAuth, toursController.postEditblog);
+
+module.exports = router; 
