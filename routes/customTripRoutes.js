233bc78ee3ceedd4144729a98aae5830c7709d3f
dejavutours <panchalchintanb@gmail.com>
2025-09-18 23:52:22 +0530

    Fix path isuse and add responce handler

--- routes/customTripRoutes.js
+++ routes/customTripRoutes.js
@@ -5,8 +5,8 @@ const isAuth = require('../middleware/is-auth');
 const customTripController = require('../controllers/customTripController');
 const ensureLoggedIn = ensureLogIn();
 router.get('/customTrip',  ensureLoggedIn, isAuth, customTripController.getCustomTrips);
-router.post('/delete/:id',  ensureLoggedIn, isAuth, customTripController.deleteCustomTrip);
-router.post('/bulk-delete',  ensureLoggedIn, isAuth, customTripController.bulkDeleteCustomTrips);
+router.post('/customTrip/delete/:id',  ensureLoggedIn, isAuth, customTripController.deleteCustomTrip);
+router.post('/customTrip/bulk-delete',  ensureLoggedIn, isAuth, customTripController.bulkDeleteCustomTrips);
 router.get('/dreamtrip', customTripController.getCustomezedTripForm);
 router.post('/customTrip', customTripController.customTrip);
 
